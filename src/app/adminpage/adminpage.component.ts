2276aea24c58597b5e781c01e6c428146a294338
Abhishek <abhishekmahajan255@gmail.com>
2023-06-05 23:37:52 +0530

    made changes in admin-page

--- src/app/adminpage/adminpage.component.ts
+++ src/app/adminpage/adminpage.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { SignupserviceService } from '../Services/signupservice.service';
-<<<<<<< HEAD
 import { Router } from '@angular/router';
-=======
->>>>>>> 0f124b59fb2ead1ed96a7d7628cd8decbb9d776a
+
+
 
 @Component({
   selector: 'cws-adminpage',
@@ -14,12 +13,8 @@ export class AdminpageComponent {
   isLoggedIn(){
     return !!this.getAccessToken();
   }
-<<<<<<< HEAD
   constructor(private signupservice:SignupserviceService,
               private router:Router){}
-=======
-  constructor(private signupservice:SignupserviceService){}
->>>>>>> 0f124b59fb2ead1ed96a7d7628cd8decbb9d776a
   
   
   logout(){
@@ -28,10 +23,9 @@ export class AdminpageComponent {
   getAccessToken(){
     return localStorage.getItem('token');
   }
-<<<<<<< HEAD
+
   isAdminPage(){
     return this.router.url=='/admin-page';
   }
-=======
->>>>>>> 0f124b59fb2ead1ed96a7d7628cd8decbb9d776a
+
 }
